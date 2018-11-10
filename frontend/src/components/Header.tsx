ef39523a1eea67f4f690f701f3c60448048ee774
andrea <andrea.kurek@fer.hr>
2018-11-10 18:33:04 +0100

    Dodan modal

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,24 +1,54 @@
 import * as React from "react";
 import { Navbar, Nav, NavDropdown, MenuItem, Glyphicon } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
+import GenreModal from 'src/containers/GenreModal';
 
 interface IState {
-    redirect: boolean;
+    logoutRedirect: boolean;
+    profileRedirect: boolean;
+    showGenreModal: boolean;
 }
 
 export default class Header extends React.Component<{}, IState> {
 
     public componentWillMount() {
-        this.setState({ redirect: false });
-    } 
-    public setRedirect = () => {
-        this.setState({ redirect: true });
+        this.setState({
+            logoutRedirect: false,
+            profileRedirect: false,
+            showGenreModal: false
+        });
+    }
+    public logout = () => {
+        this.setState({
+            logoutRedirect: true,
+        });
     };
 
-    public renderRedirect() {
-        if (this.state.redirect) {
+    public logoutRedirect() {
+        if (this.state.logoutRedirect) {
             gapi.auth2.getAuthInstance().disconnect();
             return <Redirect to="/" />;
+        }
+        return null;
+    }
+
+    public openGenreModal = () => {
+        this.setState({ showGenreModal: true })
+    }
+
+    public closeGenreModal = () => {
+        this.setState({ showGenreModal: false })
+    }
+
+    public openProfile = () => {
+        this.setState({
+            profileRedirect: true
+        })
+    }
+
+    public profileRedirect() {
+        if (this.state.profileRedirect) {
+            return <Redirect to="/profile" />;
         }
         return null;
     }
@@ -28,20 +58,22 @@ export default class Header extends React.Component<{}, IState> {
 
         return (
             <div>
-                {this.renderRedirect()}
+                {this.logoutRedirect()}
+                {this.profileRedirect()}
+                {this.state.showGenreModal && <GenreModal onClose={this.closeGenreModal} onSave={this.closeGenreModal}/>}
                 <Navbar inverse>
                     <Navbar.Header>
                         <Navbar.Brand>
-                        <a href="/movies">Ime aplikacije</a>
+                            <a href="/movies">Ime aplikacije</a>
                         </Navbar.Brand>
                     </Navbar.Header>
                     <Nav pullRight={true}>
                         <Navbar.Text>Hi, user!</Navbar.Text>
-                            <NavDropdown title={navDropdownTitle} id="basic-nav-dropdown">
-                                <MenuItem>My Profile</MenuItem>
-                                <MenuItem>Select genres</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem onSelect={this.setRedirect}>Logout</MenuItem>
+                        <NavDropdown title={navDropdownTitle} id="basic-nav-dropdown">
+                            <MenuItem onSelect={this.openProfile}>My Profile</MenuItem>
+                            <MenuItem onSelect={this.openGenreModal}>Select genres</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem onSelect={this.logout}>Logout</MenuItem>
                         </NavDropdown>
                     </Nav>
                 </Navbar>
--- frontend/src/containers/GenreModal.tsx
+++ frontend/src/containers/GenreModal.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
-export default class GenreModal extends React.Component {
+interface IProps {
+    onClose: () => void;
+    onSave: () => void;
+}
+export default class GenreModal extends React.Component<IProps> {
     public render() {
         return (
-            <div>
-                blabla
-            </div>
+            <Modal show={true} onHide={this.props.onClose}>
+                <Modal.Header closeButton={true}>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>One fine body...</Modal.Body>
+
+                <Modal.Footer>
+                    <Button onClick = {this.props.onClose}>Close</Button>
+                    <Button onClick = {this.props.onSave} bsStyle="primary">Save changes</Button>
+                </Modal.Footer>
+            </Modal>
         );
     }
 }
