060dcbfa16e94d7cb09a9c6e4f4be8f4283f33ce
Marina Šabić <marinasabic1@gmail.com>
2018-12-12 09:48:09 +0100

    css izmjenjen

--- frontend/src/components/MovieInfo.tsx
+++ frontend/src/components/MovieInfo.tsx
@@ -1,25 +1,14 @@
 import * as React from 'react';
 import '../App.css';
 import { IMovie } from 'src/utils/Typings';
-import { css } from 'emotion';
+
 import Rater from 'react-rater'
 import 'react-rater/lib/react-rater.css'
 import { Badge, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import {movieInfocontainer, movieInfobox, movieInforating } from 'src/utils/Emotions'
 
-const container = css`
-    display: grid;
-    grid-template-columns: 0.5fr 1fr;
-    padding-top: 2em;
-`
-const box = css`
-    display: grid;
-    grid-auto-rows: 50px 175px 20px;
-`
-const rating = css`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-`
+
 
 interface IProps{
     movie: IMovie;
@@ -34,16 +23,16 @@ export default class MovieInfo extends React.Component<IProps> {
             </Tooltip>
           );
         return (
-            <div className = {container}>
+            <div className = {movieInfocontainer}>
                 <Link to ={`/movies/${this.props.movie.id}`}>
                     <img src = {`http://image.tmdb.org/t/p/w185/${this.props.movie.poster_path}`} alt = "No image"/>
                 </Link>
-                <div className = {box}>
+                <div className = {movieInfobox}>
                     <div className = "movieTitle">
                         <Link to={`/movies/${this.props.movie.id}`}>{this.props.movie.title}</Link>
                     </div>
                     <div>{this.props.movie.overview}</div>
-                    <div className = {rating}>
+                    <div className = {movieInforating}>
                         <div>
                             <div>
                                 <div className = "rate">
---
+++ frontend/src/utils/Emotions.tsx
@@ -0,0 +1,15 @@
+import { css } from 'emotion';
+
+export const movieInfocontainer = css`
+    display: grid;
+    grid-template-columns: 0.5fr 1fr;
+    padding-top: 2em;
+`
+export const movieInfobox = css`
+    display: grid;
+    grid-auto-rows: 50px 175px 20px;
+`
+export const movieInforating = css`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`
