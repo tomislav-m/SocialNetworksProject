3e26283f4bd4f1f66f2efd626aef2569fe0a91e0
andrea <andrea.kurek@fer.hr>
2018-12-14 15:09:19 +0100

    Napravljen search

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -2,14 +2,39 @@ import * as React from 'react';
 import { FormGroup, FormControl, InputGroup, Button, Glyphicon } from 'react-bootstrap';
 import '../App.css';
 
-export default class Search extends React.Component {
+interface IState {
+    title: string;
+}
+
+export default class Search extends React.Component< {}, IState > {
+    
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            title: '',
+        }
+    }
+    public getMovieByTitle = () => {
+        fetch(`http://localhost:5000/api/movies/search/${this.state.title}`)
+        .then(response => response.json())
+        .then(response =>{
+            console.log(response)
+        })
+        .catch(error => console.log(error))
+    }
+
+    public handleInputChange = (event: any) => {
+        this.setState({
+            title: event.target.value
+        });
+    }
     public render() {
         return (
             <FormGroup className="searchInput">
                 <InputGroup>
-                    <FormControl type="text" placeholder="Search movies"/>
+                    <FormControl type="text" placeholder="Search movies" value={this.state.title} onChange = {this.handleInputChange}/>
                     <InputGroup.Button>
-                        <Button>
+                        <Button onClick = {this.getMovieByTitle}>
                             <Glyphicon glyph="search"/>
                         </Button>
                     </InputGroup.Button>
