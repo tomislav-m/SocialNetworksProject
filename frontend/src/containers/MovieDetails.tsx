2cd3d50f53d51273c76ea068e13a528ccc1bc9ec
vvitas <valentina.vitas@infobip.com>
2018-12-18 10:05:21 +0100

    movie details

--- frontend/src/containers/MovieDetails.tsx
+++ frontend/src/containers/MovieDetails.tsx
@@ -6,7 +6,8 @@ import { RouteComponentProps } from "react-router-dom";
 import { movieDetailsRating, infoBox, titleBox, overviewBox, movieContainer, firstMovieBox, secondMovieBox } from 'src/utils/Emotions';
 import Moment from 'react-moment';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import Rater from 'react-rater'
+import Rater from 'react-rater';
+import * as _ from 'lodash';
 
 interface IRouteParams {
     movieID: string; 
@@ -18,6 +19,8 @@ interface IProps extends RouteComponentProps<IRouteParams>{
 
 interface IState {
     loading: boolean;
+    directors: string[];
+    actors: string[];
 }
 
 @inject('mobxStore')
@@ -26,47 +29,65 @@ export default class MovieDetails extends React.Component<IProps, IState> {
     constructor(props: any){
         super(props);
         this.state ={ 
-            loading: true, 
+            loading: false, 
+            directors: new Array,
+            actors: new Array
         };
+    }
+
+    public componentDidMount(){
+        this.setState({ loading: true });
+        this.getPeople('directors', this.props.location.state.movie.directorsIds)
     }
 
     public getGenres(genreIds:string[]) {
         genreIds.forEach(genreId => {
-            console.log(genreId)
+            // console.log(genreId)
             fetch(`http://localhost:5000/api/genres/${genreId}`)
             .then(response => response.json())
             .then(response => {
-                console.log(response)
+                // console.log(response)
             })
         })
     }
 
-    public getDirectors(directorIds: string[]) {
-        directorIds.forEach(directorId => {
-            console.log(directorId)
-            fetch(`http://localhost:5000/api/people/${directorId}`)
+    public getPeople(people: string, peopleIds: string[],) {
+        const array : string[] = new Array();
+        peopleIds.forEach(id => {
+            console.log(id)
+            fetch(`http://localhost:5000/api/people/${id}`)
             .then(response => response.json())
             .then(response => {
-                console.log(response)
+                array.push(response.name);
+                people === 'directors' 
+                    ? this.setState({ directors: array})
+                    : this.setState({ actors: array })
+                this.setState({ loading: false })
             })
         })
-    }
-
-    public getActors(actorsIds: string[]) {
-        actorsIds.forEach(actorId => {
-            console.log(actorId)
-            fetch(`http://localhost:5000/api/people/${actorId}`)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response)
-            })
-        })
+        console.log('array'+ array)
     }
 
     public getSoundtrack(soundtrackId: string[]) {
         console.log(soundtrackId)
         // api za dohvaćanje soundtracka???
     }
+
+    public renderDirectors = () => {
+        console.log('renderDirectors:' + this.state.directors);
+        const forecast: any[] = new Array();
+        let key = 1;
+        _.forEach(this.state.directors, (i) => {
+            forecast.push(
+                <div key={key}>
+                    <p> <strong> { i }</strong></p>
+                </div>
+            )
+            key++;
+        });
+        return forecast;
+    }
+
 
     public render() {
         const  { movie } = this.props.location.state;
@@ -96,7 +117,7 @@ export default class MovieDetails extends React.Component<IProps, IState> {
                                 Runtime: {movie.runtime || "no information"} <br/>
                                 Genres: {this.getGenres(movie.genre_ids)} 
                             </div>
-                           
+                        
                             <div className = {movieDetailsRating}>
                                 <div>
                                     <OverlayTrigger placement="right" overlay={tooltip1}>
@@ -122,14 +143,14 @@ export default class MovieDetails extends React.Component<IProps, IState> {
                         {movie.overview} 
                     </div>
 
-                    <div>
+                    { !this.state.loading && <div>
                         Director:
-                        {this.getDirectors(movie.directorsIds)} <br/>
+                        {this.renderDirectors()}<br/>
                         Actors:
                         {this.getActors(movie.actorsIds)} <br/>
                         Soundtrack:
                         {this.getSoundtrack(movie.soundtrackId)}
-                    </div>
+                    </div>}
                 </div> 
             </div>
         );
